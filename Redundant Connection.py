47ebc89357ce7550a271c503b72690f1f5b90680
elfisworking <zymustb@126.com>
2021-01-13 17:15:58 +0800

    commit redundant connection code

--- Redundant Connection.py
+++ Redundant Connection.py
@@ -1,14 +1,23 @@
 class Solution:
     # 并查集思想
     def findRedundantConnection(self, edges):
-        
-        treeSet = set()
-        for val in edges:
-            if val[0] in treeSet and val[1] in treeSet:
-                return val
+        l = len(edges)
+        parent = list(range(l+1))
+        def find_parent(x):
+            if parent[x] == x:
+                return parent[x]
             else:
-                treeSet.add(val[0])
-                treeSet.add(val[1])
+                return find_parent(parent[x])
+        def union(x,y):
+            parent[x] = y
+        for node1, node2 in edges:
+            parent_1 = find_parent(node1)
+            parent_2 = find_parent(node2)
+            if parent_1 != parent_2:
+                union(parent_1,parent_2)
+            else:
+                return [node1,node2]
+        return []
 s = Solution()
 print(s.findRedundantConnection([[3,4],[1,2],[2,4],[3,5],[2,5]]))
         
