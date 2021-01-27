d10ef179dc168f3d77fddb25fbfc1e70b456c9b5
elfisworking <zymustb@126.com>
2021-01-27 15:44:41 +0800

    commit leetcode question 57 python code

--- 57.py
+++ 57.py
@@ -36,6 +36,29 @@ class Solution:
         if not temp:
             ans.append(newInterval)
         return ans
+    # 官方的解法 思路基本一致
+    def insert_official(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        left, right = newInterval
+        placed = False
+        ans = list()
+        for li, ri in intervals:
+            if li > right:
+                # 在插入区间的右侧且无交集
+                if not placed:
+                    ans.append([left, right])
+                    placed = True
+                ans.append([li, ri])
+            elif ri < left:
+                # 在插入区间的左侧且无交集
+                ans.append([li, ri])
+            else:
+                # 与插入区间有交集，计算它们的并集
+                left = min(left, li)
+                right = max(right, ri)
+        
+        if not placed:
+            ans.append([left, right])
+        return ans
 s = Solution()
 print(s.insert([[1,5]],[4,8]))
             
