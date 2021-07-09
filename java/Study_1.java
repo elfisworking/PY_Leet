b538c899d3dd51b0d1b2c6402d4f61f66133e42c
elfisworking <zymustb@126.com>
2021-07-09 17:22:36 +0800

    java try-with-resources

---
+++ java/Study_1.java
@@ -0,0 +1,20 @@
+package java;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Study_1 {
+    // try-with-resources 
+    // 自动的关闭资源
+    // 可以替代try-catch-finally
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(new File("test.txt"))) {
+            while(scanner.hasNext()){
+                System.out.println(scanner.nextLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
